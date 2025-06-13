03b766f35c705423e508c599157d1a78aba254e2
Arya262 <Aryarahul262@gmail.com>
2025-06-13 15:43:00 +0530

    Save local changes before pull

--- src/LoginRedirectHandler.jsx
+++ src/LoginRedirectHandler.jsx
@@ -54,11 +54,7 @@ const LoginPage = () => {
           if (data.success) {
             login(data.user); 
             console.log("User logged in:", data.user);
-<<<<<<< HEAD
-            navigate("/dashboard"); 
-=======
             navigate("/"); // redirect after login
->>>>>>> 52f1ad304b7fd7bbec33006b8bbd74ff0ab3d730
           } else {
             alert("Login failed: " + data.error);
           }
--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -4,11 +4,7 @@ import { useAuth } from "./context/AuthContext";
 const PrivateRoute = () => {
   const { user, loading } = useAuth(); // ✅ include loading
 
-<<<<<<< HEAD
-  if (loading) return <div>Loading...</div>;
-=======
   if (loading) return <div>Loading...</div>; // ✅ wait for auth check
->>>>>>> 52f1ad304b7fd7bbec33006b8bbd74ff0ab3d730
 
   if (!user) return <Navigate to="/login" replace />;
 
